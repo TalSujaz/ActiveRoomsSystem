c76d986277327ddf6d12b7c25257b4414cb7503e
Tal Sujaz <sujaztal@gmail.com>
2025-09-10 15:14:43 +0300

    fixed app views

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,8 @@ function App() {
           {/* Add later */}
         <Route path="/building/:buildingId" element={<div style={{color: 'white', padding: '2rem'}}>Building View</div>} />
         <Route path="/floor/:floorId" element={<div style={{color: 'white', padding: '2rem'}}>Floor View</div>} />
-        <Route path="/adminpanel" element={<div style={{color: 'white', padding: '2rem'}}>Floor View</div>}  />
-        <Route path="/contact" element={<div style={{color: 'white', padding: '2rem'}}>Floor View</div>} />
+        <Route path="/adminpanel" element={<div style={{color: 'white', padding: '2rem'}}>Admin Panel</div>} />
+        <Route path="/contact" element={<div style={{color: 'white', padding: '2rem'}}>Contact</div>} />
         <Route path="/staff" element={<div style={{color: 'white', padding: '2rem'}}>Staff</div>} />
         <Route path="/settings" element={<div style={{color: 'white', padding: '2rem'}}>Settings</div>} />
         <Route path="/help" element={<div style={{color: 'white', padding: '2rem'}}>Help</div>} />
